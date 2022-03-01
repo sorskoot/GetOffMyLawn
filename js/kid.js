028ca3e51dd8f4035471c38d96509a53bd339e6a
Timmy Kokke <mental_c@hotmail.com>
2022-03-01 07:48:02 +0100

    Throwing stuff

--- js/kid.js
+++ js/kid.js
@@ -1,27 +1,51 @@
-WL.registerComponent('kid', {    
-    animatedObject: {type: WL.Type.Object}
+WL.registerComponent('kid', {
+    animatedObject: { type: WL.Type.Object },
+    throwablesParent: { type: WL.Type.Object },
 }, {
-    
-    init: function() {
-        this.isSpawned = false;    
+
+    init: function () {
+        this.isSpawned = false;
         this.animation = this.animatedObject.getComponent('animation');
     },
-    
-    spawn:function(){
+
+    spawn: function () {
         this.isSpawned = true;
         this.t = 0;
         this.animation.play();
+        this.thrown = false;        
+        let num = this.throwablesParent.children.length;
+
+        this.throwing = this.throwablesParent.children[~~(Math.random()*num)];                
+        if (!this.throwing) return;
+        this.throwing.getComponent('mesh').active = false;
+        let pos = this.object.getTranslationWorld([]);
+        pos[1] = 3;
+       // pos[0] = 5.5;
+       // pos[2] = -6.5;
+        this.throwing.setTranslationWorld(pos);
     },
 
-    update:function(dt){
-        if(this.isSpawned){
-            this.t+=dt;
+    update: function (dt) {
+        if (this.isSpawned) {
+            this.t += dt;
             let pos = this.object.getTranslationWorld([]);
-            pos[1] = Math.sin(this.t / 2.0 * Math.PI ) * 1.5;
+            pos[1] = Math.sin(this.t / 2.0 * Math.PI) * 1.5;
             this.object.setTranslationWorld(pos);
+        }       
+        if (this.t > 1.2 && !this.thrown) {
+            let physx = this.throwing.getComponent('physx');
+            this.throwing.getComponent('mesh').active = true;
+            physx.kinematic = false;
+            setTimeout(()=>{
+                physx.kinematic = true;
+            }, 2500);
+            physx.addForce([0, 0, (400 +(Math.random()*400))*2 ] );
+            physx.addTorque([0,0, 500 * 2]);
+            this.throwing.parent = null;
+            this.thrown = true;
         }
 
-        if(this.t > 2.0){
+        if (this.t > 2.0) {
             this.t = 0;
             this.isSpawned = false;
             this.animation.stop();
--- js/spawner.js
+++ js/spawner.js
@@ -4,10 +4,10 @@ WL.registerComponent('spawner', {
         this.kids = this.object.children;
 
         setInterval(() => {
-            this.kids[0].getComponent('kid').spawn();
             let pos = this.kids[0].getTranslationWorld([]);
             pos[0] = (Math.random() * 10) - 3.5;
             this.kids[0].setTranslationWorld(pos);
+            this.kids[0].getComponent('kid').spawn();
         }, 3000);
     },
 
